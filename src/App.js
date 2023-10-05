fd6e0ccdab53537dbb3eef8db9645d8dec0685ce
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-05 17:04:08 +0100

    Updated Route methods to newer format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom'
-
-import Home from './pages/Home'
+import { 
+  BrowserRouter, 
+  Link, 
+  NavLink, 
+  Route, 
+  RouterProvider, 
+  Routes, 
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
+//pages
+import Home from './pages/Home';
 import About from './pages/About';
+
+//layouts
+import RootLayout from './layouts/RootLayout';
+
+const router =  createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Jobarouter</h1>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+      <RouterProvider router={router} />
   );
 }
 
