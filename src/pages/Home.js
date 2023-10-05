f63f88426435499a56a55560c34e03a09d7208ca
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-05 16:38:55 +0100

    Added Home and About Routes. Styled navigation bar

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h2>Welcome</h2>
+            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+        </div>
+    )
+}
+
+export default Home
