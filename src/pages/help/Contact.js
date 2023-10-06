d07ee8326359fe451b4e5e9ac271be214d89e4f2
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-06 11:49:45 +0100

    Using react router form component

--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Form, redirect, useActionData } from 'react-router-dom'
 
 function Contact() {
+    const data = useActionData()
     return (
         <div className='contact'>
             <h3>Contact Us</h3>
-            <form>
+            <Form method='post' action='/help/contact'>
                 <label>
                     <span>Your email</span>
                     <input type='email' name='email' required />
@@ -14,9 +16,26 @@ function Contact() {
                     <textarea name='message' required></textarea>
                 </label>
                 <button>Submit</button>
-            </form>
+                {data && data.error && <p>{data.error}</p>}
+            </Form>
         </div>
     )
 }
 
 export default Contact
+
+export const contactAction = async ({ request }) => {
+
+    const data = await request.formData()
+
+    const submission = {
+        email: data.get('email'),
+        message: data.get('message')
+    }
+    if (submission.message.length < 10) {
+        return {error: 'Message must be over 10 chars long!'}
+    }
+
+    //redirect user
+    return redirect('/')
+}
